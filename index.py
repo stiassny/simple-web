0c48c195aefb102df3a2d9aaa03ff3b3932f751b
stiassny <35328381+stiassny@users.noreply.github.com>
2018-03-15 18:12:41 +0200

    Update index.py

--- index.py
+++ index.py
@@ -39,17 +39,15 @@ class HandlerClass(SimpleHTTPRequestHandler):
             del request[addr_pair]
             request[addr_pair]=[num,ts]
         file=open("index.html", "w")
-        file.write("<!DOCTYPE html> <html> <body><center><h1><font color=\"blue\" face=\"Georgia, Arial\" size=8><em>Real</em></font> Visit Results</h1></center>");
         for pair in request:
             if pair[0] == host:
                 guest = "LOCAL: "+pair[0]
             else:
                 guest = pair[0]
             if (time_now-datetime.strptime(request[pair][1],'%Y-%m-%d %H:%M:%S')).seconds < 3:
-                file.write("<p style=\"font-size:150%\" >#"+ str(request[pair][1]) +": <font color=\"red\">"+str(request[pair][0])+ "</font> requests " + "from &lt<font color=\"blue\">"+guest+"</font>&gt to WebServer &lt<font color=\"blue\">"+pair[1]+"</font>&gt</p>")
+                file.write(pair[1])
             else:
-                file.write("<p style=\"font-size:150%\" >#"+ str(request[pair][1]) +": <font color=\"maroon\">"+str(request[pair][0])+ "</font> requests " + "from &lt<font color=\"navy\">"+guest+"</font>&gt to WebServer &lt<font color=\"navy\">"+pair[1]+"</font>&gt</p>")
-        file.write("</body> </html>");
+                file.write(pair[1])
         file.close()
         pickle.dump(request,open("pickle_data.txt","w"))
 
